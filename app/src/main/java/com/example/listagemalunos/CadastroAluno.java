38942380bffa80ab63de9ef5231a831b1135d783
Lucas Reis <lucaskast03@gmail.com>
2019-10-09 22:28:45 -0300

    Project Class Finished

---
+++ app/src/main/java/com/example/listagemalunos/CadastroAluno.java
@@ -0,0 +1,55 @@
+package com.example.listagemalunos;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import java.lang.reflect.Array;
+
+public class cadastroAluno extends AppCompatActivity {
+
+    Spinner listaCursos;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cadastro_aluno);
+
+        listaCursos = findViewById(R.id.spnCursos);
+
+        String [] cursos = getResources().getStringArray(R.array.Cursos);
+
+        ArrayAdapter<String> adpt = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, cursos);
+
+        listaCursos.setAdapter(adpt);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_cadastro_aluno, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        switch (item.getItemId()){
+            case R.id.menuSalvar: {
+                break;
+            }
+
+            case R.id.menuCancelar: {
+                finish();
+            }
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
---
+++ app/src/main/java/com/example/listagemalunos/ListagemAlunos.java
@@ -0,0 +1,50 @@
+package com.example.listagemalunos;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Switch;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_listagem_aluno, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.novoAluno) {
+            Intent i = new Intent(this, cadastroAluno.class);
+            startActivity(i);
+        } else if (id == R.id.menuFechar) {
+            finish();
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
+
+
+
+
+
+
+
+
